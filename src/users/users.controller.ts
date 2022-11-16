98825565ba46a133e90332021b0be9e45a4d3784
Joe <joe.syx@gmail.com>
2022-11-16 22:17:02 +1100

    Create and save the user data.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,52 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Patch,
+  Get,
+  Param,
+  Post,
+  Query,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateuserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(private usersService: UsersService) {}
+
   @Post('/signup')
   createUser(@Body() body: CreateuserDto) {
-    console.log(body);
+    const { email, password } = body;
+
+    return this.usersService.create(email, password);
+  }
+
+  @Get('/:id')
+  async findUser(@Param('id') id: string) {
+    const user = await this.usersService.findOne(parseInt(id));
+
+    if (!user) {
+      throw new NotFoundException('User is not found');
+    }
+
+    return user;
+  }
+
+  @Get()
+  findAllUsers(@Query('email') email: string) {
+    return this.usersService.find(email);
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id') id: string) {
+    return this.usersService.remove(parseInt(id));
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.usersService.update(parseInt(id), body);
   }
 }
